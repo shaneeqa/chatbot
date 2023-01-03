2e9447625e5bc2eccc5174163ffe8ddc5479cda4
shaneeqa <37417454+shaneeqa@users.noreply.github.com>
2023-01-03 21:11:21 +0530

    working upto chat in the backend

--- chat.py
+++ chat.py
@@ -1,6 +1,14 @@
 import torch
 from model import NeuralNet
 from preprocessor import tokenize, bag_of_words
+import json
+import random
+
+
+with open("intents.json",'r') as f:
+    intents_data = json.load(f)
+    intents = intents_data['intents']
+
 
 FILE = "model_data.pth"
 
@@ -33,7 +41,29 @@ def response(userInput):
     sentence = tokenize(userInput)
     
     X = bag_of_words(sentence, all_words)
-    print(X)
+    X = X.reshape(1, X.shape[0]) # create an array of array - num py array
+    
+    X = torch.from_numpy(X).to(device) # converting to torch tensor
+    
+    out = model(X)
+
+    #find out the probability
+    _, predicted = torch.max(out, dim = 1)
+
+    tag = tags[predicted.item()]
+
+    # threshold 75%
+    probabilitiess = torch.softmax(out, dim = 1) 
+
+    probabilityy = probabilitiess[0][predicted.item()]
+
+    if probabilityy.item() > 0.75:
+        for intent in intents:
+            if tag == intent['tag']:
+                return random.choice(intent['responses'])
+
+    return "I don't understand, kindly rephrase"
+
 
 if __name__ == "__main__":
     print("Let's start chatting! type (quit) to exit the chat")
